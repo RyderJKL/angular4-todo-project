6de1fa4aea1be87ff2cc4403ea024a4d93bc8057
onejustone404 <807527097@qq.com>
2017-06-06 09:57:35 +0800

    1.使用ElementRef时需要类型转换,input$ = Observable.fromEvent<KeyboardEvent>(this.el.nativeElement.querySelector('#play-jack'),'input') 2.使用 rxjs.map 发射输入框的值时需要加入类型装换, input$. map((e:any) => e.target.value)

--- src/app/playground/playground.component.ts
+++ src/app/playground/playground.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,AfterViewInit,Inject,AfterContentInit,ElementRef,ViewChild} from '@angular/core';
+import {Observable} from "rxjs/Observable";
+import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/scan'
+import 'rxjs/add/operator/filter'
+import 'rxjs/add/operator/throttleTime'
+
 
 @Component({
   selector: 'app-playground',
@@ -7,9 +13,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlaygroundComponent implements OnInit {
 
-  constructor() { }
+  // @ViewChild('myname') input;
+
+  constructor(
+    private el: ElementRef,
+  ) {
+
+
+  }
+
+  ngAfterViewInit() {
+    let input$ = Observable.fromEvent<KeyboardEvent>(this.el.nativeElement.querySelector('#play-jack'),'input')
+      .map((e:any) => e.target.value)
+      .filter(value => value.length >=1)
+      .throttleTime(100)
+      .distinctUntilChanged()
+    input$.subscribe(next =>
+      console.log(next),
+        error => console.log(error)
+    )
+  }
+
+  ngAfterContentInit(){
+    console.log('content')
+  }
 
   ngOnInit() {
+    // console.log()
+
+
   }
+
 
 }
