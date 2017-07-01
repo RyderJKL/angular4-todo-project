45c88d8456a3823a1823eb87a91722ed616d9872
onejustone404 <807527097@qq.com>
2017-07-01 15:40:04 +0800

    模板驱动表单，添加自定义验证器validateEqual

--- src/app/register/directive/repeat-validator.directive.ts
+++ src/app/register/directive/repeat-validator.directive.ts
@@ -1,3 +1,65 @@
 /**
  * Created by root on 17-7-1.
  */
+import {Directive,forwardRef,Attribute} from '@angular/core'
+
+import {NG_VALIDATORS,Validator,AbstractControl} from '@angular/forms'
+
+@Directive({
+  selector:'[validateEqual][ngModel]',
+  // 除了要求 DOM 元素应用 validateEqual 之外，还需要它是一个 ngModel 元素，这样它才是一个 FormControl，
+  providers: [
+    {
+      provide: NG_VALIDATORS,
+      useExisting: forwardRef(() => RepeatValidatorDirective),
+      // 因为我们需要在元数据中使用还为未生成的 RepeatValidatorDirective,所以需要使用forwardRef 延迟执行。其接受一个返回一个类的函数作为参数，但这个函数不会立即被调用，而是在该类声明后被调用，也就避免了 undefined
+      multi: true
+    }
+  ]
+})
+
+export class RepeatValidatorDirective implements Validator{
+  constructor(
+    @Attribute('validateEqual') public validateEqual: string,
+    @Attribute('reverse') public reverse:string
+    // reverse 用于确认，密码与确认密码之间的主从关系
+  ){}
+
+  private get isReverse(){
+      return this.reverse === 'true';
+  }
+
+  validate(c: AbstractControl): {[key:string]:any} {
+    // validate 必须实现的方法,验证正确返回 null 否则返回一个对象
+
+    const self = c.value;
+    // 控件自身值
+
+    const target = c.root.get(this.validateEqual);
+    //要对比的值，也就是在 validateEqual=“ctrlname” 的那个控件的值
+
+    if(target && self !== target.value && !this.isReverse){
+      // 不反向查询且值不相等
+      return {
+        validateEqual: true
+      }
+    }
+
+    if (target && self === target.value && this.isReverse){
+      //反向查询且值相等
+      delete target.errors['validateEqual']
+      if (!Object.keys(target.errors).length) {
+        target.setErrors(null);
+      }
+    }
+
+    if(target && self !== target.value && this.isReverse){
+      // 反向查询且值不等
+      target.setErrors({
+        validateEqual: true
+      })
+    }
+
+    return null;
+  }
+}
--- src/app/register/register-routing.module.ts
+++ src/app/register/register-routing.module.ts
@@ -5,7 +5,6 @@ import {NgModule} from '@angular/core'
 import {RouterModule,Routes} from '@angular/router'
 import {RegisterComponent} from './register.component'
 
-
 const routes: Routes = [
   {
     path: '',
@@ -15,7 +14,8 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+
 })
 
 export class RegisterRoutingModule {}
